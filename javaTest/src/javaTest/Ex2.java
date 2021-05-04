6385a5fa6ff1851ac85145ef8a3d897056dc7e24
LeeKH <rmsgh0726@naver.com>
2021-05-04 15:56:49 +0900

    연습중

---
+++ javaTest/src/javaTest/Ex2.java
@@ -0,0 +1,16 @@
+package javaTest;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		int i  = 10; // 변수의 초기화
+		int result;
+		result = i + 1;
+		System.out.println(result);
+		int j;
+		j = 10; // 값의 대입
+		System.out.println(j);
+
+	}
+
+}
